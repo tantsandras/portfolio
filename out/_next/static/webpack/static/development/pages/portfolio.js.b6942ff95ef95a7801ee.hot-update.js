bcd67f668ebd0dce6234db6f2ff85ab3e1a5a203
Sandra <grahlsan@gmail.com>
2019-04-30 11:44:42 +0100

    ran next export

---
+++ out/_next/static/webpack/static/development/pages/portfolio.js.b6942ff95ef95a7801ee.hot-update.js
@@ -0,0 +1,46 @@
+webpackHotUpdate("static/development/pages/portfolio.js",{
+
+/***/ "./components/projectGifs.js":
+/*!***********************************!*\
+  !*** ./components/projectGifs.js ***!
+  \***********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+var _jsxFileName = "/home/sandra/projects/next-js/components/projectGifs.js";
+
+
+
+var ProjectGif = function ProjectGif(_ref) {
+  var props = _ref.props;
+  return react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("video", {
+    width: "500px",
+    height: "250",
+    autoPlay: true,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("source", {
+    src: props,
+    type: "video/mp4",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (ProjectGif);
+
+/***/ })
+
+})
+//# sourceMappingURL=portfolio.js.b6942ff95ef95a7801ee.hot-update.js.map
